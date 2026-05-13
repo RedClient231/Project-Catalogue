f2beb34d44db32751fbac1a60135c494bbeb5ccc
RedClient231 <RedClient231@users.noreply.github.com>
2026-05-14 01:43:43 +0800

    Fix: add plugin versions, fix cmake version in app/build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.6" apply false
 }
